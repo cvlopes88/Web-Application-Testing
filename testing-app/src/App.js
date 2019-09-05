0ea56d28b0ad9b381567713adf6409a9d8f3361e
unknown <cvlopes88@gmail.com>
2019-09-05 18:08:37 -0400

    1st

---
+++ testing-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Display from './Display';
+import Dashboard from './Dashboard';
+function App() {
+  
+
+
+
+
+  return (
+    <div className="App">
+      <h1>SCORE BOARD</h1>
+     {/* <Display /> */}
+     <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ testing-app/src/Dashboard.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Display from './Display'
+ const Dashboard = () => {
+    const [balls, setBalls] = useState(0);
+    const [strikes, setStrikes] = useState(0);
+
+
+    
+  if (balls > 4) {
+    setBalls(0);
+  }
+  
+  if (strikes > 3) {
+    setStrikes(0);
+  }
+
+const hitScore = () => {
+    setStrikes(0);
+    setBalls(0);
+}
+
+
+const foulHit = () => {
+     if (strikes === 0){
+         setStrikes(strikes + 1)
+        }else if (strikes === 1){
+        setStrikes(strikes + 1)
+        }else if (strikes === 2){
+        setStrikes(2)
+
+    }
+}
+
+
+
+  return (
+      <div>
+          <Display propsDisplay={balls} propsStrike={strikes} />
+          <div>
+              <button
+              onClick={() => setBalls(balls + 1) }
+              >Ball</button>
+          </div>
+          <div>
+              <button
+              onClick={() => setStrikes(strikes + 1) }
+              >Strike</button>
+          </div>
+          <div>
+              <button
+              onClick={foulHit}
+              >Foul</button>
+          </div>
+          <div>
+              <button
+              onClick={hitScore}
+              >Hit</button>
+          </div>
+           
+      </div>
+  )
+
+
+
+
+ }
+ export default Dashboard;
